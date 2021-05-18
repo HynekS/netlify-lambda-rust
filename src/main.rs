03499188e9e715a41ac081b1b6132cfc84614076
HynekS <hyneksvacha@seznam.cz>
2021-05-18 21:02:59 +0200

    converted headers to hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use netlify_lambda_http::{handler, lambda::{self, Context}, IntoResponse, Request, RequestExt};
 use serde_json::{json};
+use http::{header::HeaderValue, HeaderMap};
+use std::collections::HashMap;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+fn convert(headers: &HeaderMap<HeaderValue>) -> HashMap<String, Vec<String>> {
+    let mut header_hashmap = HashMap::new();
+    for (k, v) in headers {
+        let k = k.as_str().to_owned();
+        let v = String::from_utf8_lossy(v.as_bytes()).into_owned();
+        header_hashmap.entry(k).or_insert_with(Vec::new).push(v)
+    }
+    header_hashmap
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -30,5 +42,5 @@ async fn hello(
 ) -> Result<impl IntoResponse, Error> {
     let b = request.headers();
     let uri = request.uri().path().split("/").collect::<Vec<&str>>();
-    Ok(json!({ "request": b, "uriSegments": uri }))
+    Ok(json!({ "request": convert(b), "uriSegments": uri }))
 }
