e5711f5112c5c76b543bc7f82b320f60e18b5e75
HynekS <hyneksvacha@seznam.cz>
2021-05-18 20:41:20 +0200

    returning json

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use netlify_lambda_http::{handler, lambda::{self, Context}, IntoResponse, Request, RequestExt};
+use serde_json::{json};
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
@@ -7,7 +8,7 @@ async fn main() -> Result<(), Error> {
     lambda::run(handler(hello)).await?;
     Ok(())
 }
-
+/*
 async fn hello(
     request: Request,
     _: Context
@@ -20,4 +21,14 @@ async fn hello(
             .unwrap_or_else(|| "stranger"),
         request.uri()
     ))
+}
+*/
+
+async fn hello(
+    request: Request,
+    _: Context
+) -> Result<impl IntoResponse, Error> {
+    let b = request.body();
+    let uri = request.uri().path().split("/").collect::<Vec<&str>>();
+    Ok(json!({ "request": b, "uriSegments": uri }))
 }
